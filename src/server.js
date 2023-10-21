8995357325e1ab3e03dedeca468baa4793003948
Omar Lamrani <lamraniomarcs@gmail.com>
2023-10-21 23:17:00 +0100

    added pp to home

--- src/server.js
+++ src/server.js
@@ -7,10 +7,12 @@ app.engine('ejs', require('ejs').__express)
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'ejs');
 
+app.use(express.static(__dirname + '/../public/images'));
+
 app.use(bodyParser.urlencoded({ extended: false})); // add http parsing to server
 app.use('/', require('./routes'));
 
 app.listen(port, () => { // app.listen = server init at port + callback (functions called when init complete)
 	console.log(`Success! Your application is running on port ${port}.`);
+	console.log(__dirname + `/../public`);
 });
-
